93c8f55a5a3b551b6ac06d8a4098ed2bd3b45a66
Hakan <97197833+Dro-Dro@users.noreply.github.com>
2024-09-18 15:32:19 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+
+client = OpenAI(
+    api_key = 'KEY'
+)
+
+messages=[]
+
+# user input
+user_input = input('Enter your prompt: ')
+
+# create prompt
+messages.append({'role': 'user', 'content': user_input})
+
+# send the api call
+response = client.chat.completions.create(messages = messages, model = "gpt-3.5-turbo", max_tokens = 2)
+
+# display response in console
+print(response.choices[0].message.content)
