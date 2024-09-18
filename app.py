87340af1b0315c092dc4a2b21fd00d50f66cc443
Hakan <97197833+Dro-Dro@users.noreply.github.com>
2024-09-18 16:12:15 -0700

    bot can now quiz

--- app.py
+++ app.py
@@ -4,19 +4,16 @@ client = OpenAI(
     api_key = 'KEY'
 )
 
-messages=[]
+messages = []
+prompt = {'role': 'user', 'content': 'You are a quizzer. Present the user with a multiple choice question to practice for a BLANK, they have to respond by typing a, b, c, d or e. Wait until the user respond before listing another related question.'}
+# replace BLANK with quiz subject (for some reason keeping it as BLANK prompts the AI to give geography related questions)
+# provide the user with a quiz question
+# 5 possible answers via mult-choice question
+# answer should be correct or not
+# then present another question
+messages.append(prompt)
 
 while True:
-    # user input
-    user_input = input('Enter your prompt: ')
-
-    # quit chat if user types q
-    if user_input == 'q':
-        exit()
-
-    # create prompt
-    messages.append({'role': 'user', 'content': user_input})
-
     # send the api call
     response = client.chat.completions.create(messages = messages, model = "gpt-3.5-turbo")
 
@@ -24,5 +21,17 @@ while True:
     print(response.choices[0].message.content)
 
     # past conversation printed
-    messages.append(response.choices[0])
-    print(messages)
+    messages.append(response.choices[0].message)
+
+    # send prompt again to continue quiz
+    messages.append(prompt)
+
+    # user input
+    user_input = input('Enter your answer: ')
+
+    # quit chat if user types q
+    if user_input == 'q':
+        exit()
+
+    # create prompt
+    messages.append({'role': 'user', 'content': user_input})
